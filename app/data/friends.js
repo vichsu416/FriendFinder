d435f8e51d2cce93e0adb3aa711a4cc1dc9b74f7
Tzu-Tang Hsu <vichsu416@gmail.com>
2017-06-28 12:10:00 -0400

    2nd change

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+//data hold the list of possible friends who also did the survey
+
+var friendsArray = [
+	{
+
+	
+	}
+];
+
+
+//export this array, so make it accessible through other file using require
+module.exports = friendsArray;
